67515652fda2e92d46a2bcac4f41d258aedce780
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-11-30 01:46:21 +0100

    animated_emoji_module: Use emoji strings from Dice class

--- homebot/modules/animated_emoji.py
+++ homebot/modules/animated_emoji.py
@@ -2,18 +2,20 @@ from homebot import get_config
 from homebot.logging import LOGE, LOGI, LOGD, LOGW
 from homebot.modules_manager import register
 
+from telegram.dice import Dice
+
 @register(commands=['basket'])
 def basket(update, context):
-	update.message.reply_dice(emoji="🏀")
+	update.message.reply_dice(emoji=Dice.BASKETBALL)
 
 @register(commands=['dart'])
 def dart(update, context):
-	update.message.reply_dice(emoji="🎯")
+	update.message.reply_dice(emoji=Dice.DARTS)
 
 @register(commands=['dice'])
 def dice(update, context):
-	update.message.reply_dice(emoji="🎲")
+	update.message.reply_dice(emoji=Dice.DICE)
 
 @register(commands=['football'])
 def football(update, context):
-	update.message.reply_dice(emoji="⚽️")
+	update.message.reply_dice(emoji=Dice.FOOTBALL)
