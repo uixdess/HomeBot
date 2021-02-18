22b27c2fac02e6ec85b037cd432ebcf682d3c1c6
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-18 23:20:15 +0100

    main: Remove useless connection check on start
    * The library is smart enough to do that by itself

--- homebot/main.py
+++ homebot/main.py
@@ -1,14 +1,9 @@
 from homebot import __version__, get_config
 from homebot.core.bot import Bot
-from homebot.core.logging import LOGE, LOGI
+from homebot.core.logging import LOGI
 
 def main():
 	bot = Bot(get_config("BOT_API_TOKEN"))
-	try:
-		bot.updater.bot.get_me().username
-	except:
-		LOGE("Failed to connect to Telegram, check your internet connection and/or your bot token")
-		exit()
 	LOGI("HomeBot started, version " + str(__version__))
 	LOGI("Bot username: @" + bot.updater.bot.get_me().username)
 	bot.updater.start_polling()
