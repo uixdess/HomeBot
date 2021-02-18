d98cdf0d82ab9b48fa8232789ece9afc1f31d6a3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-18 23:21:21 +0100

    main: Move to string formatting

--- homebot/main.py
+++ homebot/main.py
@@ -4,6 +4,6 @@ from homebot.core.logging import LOGI
 
 def main():
 	bot = Bot(get_config("BOT_API_TOKEN"))
-	LOGI("HomeBot started, version " + str(__version__))
-	LOGI("Bot username: @" + bot.updater.bot.get_me().username)
+	LOGI(f"HomeBot started, version {__version__}")
+	LOGI(f"Bot username: @{bot.updater.bot.get_me().username}")
 	bot.updater.start_polling()
