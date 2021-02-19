b7606439cabb0deee541e0a5d6da260c96039790
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-19 01:04:23 +0100

    ci_module: Add some newlines

--- homebot/modules/ci/__init__.py
+++ homebot/modules/ci/__init__.py
@@ -11,15 +11,19 @@ def ci(update, context):
 		update.message.reply_text("Error: You are not authorized to use CI function of this bot.\n"
 								  "Ask to who host this bot to add you to the authorized people list")
 		return
+
 	if get_config("CI_CHANNEL_ID") == "":
 		update.message.reply_text("Error: CI channel or user ID not defined")
 		LOGE("CI channel or user ID not defined")
 		return
+
 	project = update.message.text.split()[1]
 	if not os.path.isfile(bot_path / "modules" / "ci" / "projects" / (project + ".py")):
 		update.message.reply_text("Error: Project script not found")
 		return
+
 	project_module = import_module('homebot.modules.ci.projects.' + project, package="*")
+
 	LOGI("CI workflow started, project: " + project)
 	project_module.ci_build(update, context)
 	LOGI("CI workflow finished, project: " + project)
