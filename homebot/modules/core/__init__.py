356a9b667553fd02cee54625ba3dbaebb024455d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-17 10:44:52 +0100

    core_module: Updates for new modules manager

--- homebot/modules/core/__init__.py
+++ homebot/modules/core/__init__.py
@@ -2,78 +2,83 @@
 
 from homebot import __version__, get_config
 from homebot.core.admin import user_is_admin
-from homebot.core.bot import get_bot_context
 from homebot.core.logging import LOGI
+from homebot.core.modules_manager import ModuleBase
 from telegram.ext import CallbackContext
 from telegram.update import Update
 
-def start(update: Update, context: CallbackContext):
-	update.message.reply_text("Hi! I'm HomeBot, and I'm alive\n"
-							  f"Version {__version__}\n"
-							  "To see all the available modules, type /modules")
+class Module(ModuleBase):
+	name = "core"
+	description = "Core functions of the bot"
+	version = "1.0.0"
 
-def modules(update: Update, context: CallbackContext):
-	message = "Loaded modules:\n\n"
-	modules = get_bot_context().modules
-	for module in modules:
-		message += f"{module.name}\n"
-		message += f"Status: {modules[module]}\n"
-		message += f"Commands: {', '.join([command.name for command in module.commands])}\n\n"
-	update.message.reply_text(message)
+	def start(self, update: Update, context: CallbackContext):
+		update.message.reply_text("Hi! I'm HomeBot, and I'm alive\n"
+								  f"Version {__version__}\n"
+								  "To see all the available modules, type /modules")
 
-def load(update: Update, context: CallbackContext):
-	if not user_is_admin(update.message.from_user.id):
-		update.message.reply_text("Error: You are not authorized to load modules")
-		return
+	def modules(self, update: Update, context: CallbackContext):
+		message = "Loaded modules:\n\n"
+		modules = self.bot.modules
+		for module in modules:
+			message += f"{module.name}\n"
+			message += f"Status: {module.status}\n"
+			message += f"Commands: {', '.join([command.name for command in module.commands])}\n\n"
+		update.message.reply_text(message)
 
-	try:
-		module_name = update.message.text.split(' ', 1)[1]
-	except IndexError:
-		update.message.reply_text("Error: Module name not provided")
-		return
-
-	if module_name == "core":
-		update.message.reply_text("Error: You can't load module used for loading/unloading modules")
-		return
-
-	bot_context = get_bot_context()
-	modules = bot_context.modules
-	for module in modules:
-		if module_name == module.name:
-			bot_context.load_module(module)
-			update.message.reply_text(f"Module {module_name} loaded")
+	def load(self, update: Update, context: CallbackContext):
+		if not user_is_admin(update.message.from_user.id):
+			update.message.reply_text("Error: You are not authorized to load modules")
 			return
 
-	update.message.reply_text("Error: Module not found")
-
-def unload(update: Update, context: CallbackContext):
-	if not user_is_admin(update.message.from_user.id):
-		update.message.reply_text("Error: You are not authorized to unload modules")
-		return
-
-	try:
-		module_name = update.message.text.split(' ', 1)[1]
-	except IndexError:
-		update.message.reply_text("Error: Module name not provided")
-		return
-
-	if module_name == "core":
-		update.message.reply_text("Error: You can't unload module used for loading/unloading modules")
-		return
-
-	bot_context = get_bot_context()
-	modules = bot_context.modules
-	for module in modules:
-		if module_name == module.name:
-			bot_context.unload_module(module)
-			update.message.reply_text(f"Module {module_name} unloaded")
+		try:
+			module_name = update.message.text.split(' ', 1)[1]
+		except IndexError:
+			update.message.reply_text("Error: Module name not provided")
 			return
 
-	update.message.reply_text("Error: Module not found")
+		if module_name == "core":
+			update.message.reply_text("Error: You can't load module used for loading/unloading modules")
+			return
 
-commands = {
-	start: ['start', 'help'],
-	modules: ['modules'],
-	load: ['load'],
-	unload: ['unload']
-}
+		bot_context = self.bot
+		modules = bot_context.modules
+		for module in modules:
+			if module_name == module.name:
+				bot_context.load_module(module)
+				update.message.reply_text(f"Module {module_name} loaded")
+				return
+
+		update.message.reply_text("Error: Module not found")
+
+	def unload(self, update: Update, context: CallbackContext):
+		if not user_is_admin(update.message.from_user.id):
+			update.message.reply_text("Error: You are not authorized to unload modules")
+			return
+
+		try:
+			module_name = update.message.text.split(' ', 1)[1]
+		except IndexError:
+			update.message.reply_text("Error: Module name not provided")
+			return
+
+		if module_name == "core":
+			update.message.reply_text("Error: You can't unload module used for loading/unloading modules")
+			return
+
+		bot_context = self.bot
+		modules = bot_context.modules
+		for module in modules:
+			if module_name == module.name:
+				bot_context.unload_module(module)
+				update.message.reply_text(f"Module {module_name} unloaded")
+				return
+
+		update.message.reply_text("Error: Module not found")
+
+	commands = {
+		start: ['start', 'help'],
+		modules: ['modules'],
+		load: ['load'],
+		unload: ['unload']
+	}
