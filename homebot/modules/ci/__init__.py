6642f96020cd628cd327bf2b4d32decaaca8bd67
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-17 10:44:40 +0100

    ci_module: Updates for new modules manager

--- homebot/modules/ci/__init__.py
+++ homebot/modules/ci/__init__.py
@@ -3,39 +3,45 @@
 from homebot import get_config
 from homebot.core.admin import user_is_admin
 from homebot.core.logging import LOGE, LOGI
+from homebot.core.modules_manager import ModuleBase
 from homebot.modules.ci.parser import CIParser
 from importlib import import_module
 from telegram.ext import CallbackContext
 from telegram.update import Update
 
-def ci(update: Update, context: CallbackContext):
-	if not user_is_admin(update.message.from_user.id):
-		update.message.reply_text("Error: You are not authorized to use CI function of this bot.\n"
-								  "Ask to who host this bot to add you to the authorized people list")
-		return
+class Module(ModuleBase):
+	name = "ci"
+	description = "A module that let you trigger actions with a single Telegram message"
+	version = "1.0.0"
 
-	if get_config("CI_CHANNEL_ID") == "":
-		update.message.reply_text("Error: CI channel or user ID not defined")
-		LOGE("CI channel or user ID not defined")
-		return
+	def ci(self, update: Update, context: CallbackContext):
+		if not user_is_admin(update.message.from_user.id):
+			update.message.reply_text("Error: You are not authorized to use CI function of this bot.\n"
+									  "Ask to who host this bot to add you to the authorized people list")
+			return
 
-	parser = CIParser(prog="/ci")
-	parser.set_output(update.message.reply_text)
-	parser.add_argument('project', help='CI project')
+		if get_config("CI_CHANNEL_ID") == "":
+			update.message.reply_text("Error: CI channel or user ID not defined")
+			LOGE("CI channel or user ID not defined")
+			return
 
-	args_passed = update.message.text[len("/ci"):].split()
-	args, _ = parser.parse_known_args(args_passed)
+		parser = CIParser(prog="/ci")
+		parser.set_output(update.message.reply_text)
+		parser.add_argument('project', help='CI project')
 
-	try:
-		project_module = import_module('homebot.modules.ci.projects.' + args.project, package="*")
-	except ImportError:
-		update.message.reply_text("Error: Project script not found")
-		return
+		args_passed = update.message.text[len("/ci"):].split()
+		args, _ = parser.parse_known_args(args_passed)
 
-	LOGI("CI workflow started, project: " + args.project)
-	project_module.ci_build(update, context)
-	LOGI("CI workflow finished, project: " + args.project)
+		try:
+			project_module = import_module('homebot.modules.ci.projects.' + args.project, package="*")
+		except ImportError:
+			update.message.reply_text("Error: Project script not found")
+			return
 
-commands = {
-	ci: ['ci']
-}
+		LOGI("CI workflow started, project: " + args.project)
+		project_module.ci_build(update, context)
+		LOGI("CI workflow finished, project: " + args.project)
+
+	commands = {
+		ci: ['ci']
+	}
