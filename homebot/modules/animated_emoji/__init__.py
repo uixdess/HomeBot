313f47146acf27c4d50a69facd44d00d1442a4ac
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-17 10:43:52 +0100

    animated_emoji_module: Updates for new modules manager

--- homebot/modules/animated_emoji/__init__.py
+++ homebot/modules/animated_emoji/__init__.py
@@ -1,28 +1,34 @@
 """HomeBot animated emoji module."""
 
+from homebot.core.modules_manager import ModuleBase
 from telegram.dice import Dice
 from telegram.ext import CallbackContext
 from telegram.update import Update
 
-def basket(update: Update, context: CallbackContext):
-	update.message.reply_dice(emoji=Dice.BASKETBALL)
+class Module(ModuleBase):
+	name = "animated_emoji"
+	description = "Send animated emojis with a single command"
+	version = "1.0.0"
 
-def dart(update: Update, context: CallbackContext):
-	update.message.reply_dice(emoji=Dice.DARTS)
+	def basket(self, update: Update, context: CallbackContext):
+		update.message.reply_dice(emoji=Dice.BASKETBALL)
 
-def dice(update: Update, context: CallbackContext):
-	update.message.reply_dice(emoji=Dice.DICE)
+	def dart(self, update: Update, context: CallbackContext):
+		update.message.reply_dice(emoji=Dice.DARTS)
 
-def football(update: Update, context: CallbackContext):
-	update.message.reply_dice(emoji=Dice.FOOTBALL)
+	def dice(self, update: Update, context: CallbackContext):
+		update.message.reply_dice(emoji=Dice.DICE)
 
-def slotmachine(update: Update, context: CallbackContext):
-	update.message.reply_dice(emoji=Dice.SLOT_MACHINE)
+	def football(self, update: Update, context: CallbackContext):
+		update.message.reply_dice(emoji=Dice.FOOTBALL)
 
-commands = {
-	basket: ['basket'],
-	dart: ['dart'],
-	dice: ['dice'],
-	football: ['football'],
-	slotmachine: ['slotmachine']
-}
+	def slotmachine(self, update: Update, context: CallbackContext):
+		update.message.reply_dice(emoji=Dice.SLOT_MACHINE)
+
+	commands = {
+		basket: ['basket'],
+		dart: ['dart'],
+		dice: ['dice'],
+		football: ['football'],
+		slotmachine: ['slotmachine']
+	}
