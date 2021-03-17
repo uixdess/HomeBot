df45224dab77dcde1663f512019ce24dbb47740d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-17 10:45:02 +0100

    cowsay_module: Updates for new modules manager

--- homebot/modules/cowsay/__init__.py
+++ homebot/modules/cowsay/__init__.py
@@ -2,19 +2,25 @@
 
 from contextlib import redirect_stdout
 from cowsay import cow
+from homebot.core.modules_manager import ModuleBase
 import io
 from telegram.ext import CallbackContext
 from telegram.update import Update
 
-def cowsay(update: Update, context: CallbackContext):
-	with io.StringIO() as buf, redirect_stdout(buf):
-		try:
-			cow(update.message.text.split(' ', 1)[1])
-		except IndexError:
-			update.message.reply_text("Error: Write something after the command!")
-		else:
-			update.message.reply_text(f"`{buf.getvalue()}`", parse_mode="Markdown")
+class Module(ModuleBase):
+	name = "cowsay"
+	description = "Moo"
+	version = "1.0.0"
 
-commands = {
-	cowsay: ['cowsay']
-}
+	def cowsay(update: Update, context: CallbackContext):
+		with io.StringIO() as buf, redirect_stdout(buf):
+			try:
+				cow(update.message.text.split(' ', 1)[1])
+			except IndexError:
+				update.message.reply_text("Error: Write something after the command!")
+			else:
+				update.message.reply_text(f"`{buf.getvalue()}`", parse_mode="Markdown")
+
+	commands = {
+		cowsay: ['cowsay']
+	}
