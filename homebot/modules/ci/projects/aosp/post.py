dc72d6acaf39aecbfbfdfc32efac7c43641d9196
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-27 16:25:28 +0100

    ci_module: aosp: post: Workaround a python-telegram-bot bug
    * Editing message times out but after 1-2 seconds the message is edited anyway

--- homebot/modules/ci/projects/aosp/post.py
+++ homebot/modules/ci/projects/aosp/post.py
@@ -1,5 +1,6 @@
 from homebot import get_config
 from homebot.modules.ci.projects.aosp.project import AOSPProject
+from telegram.error import TimedOut
 from telegram.ext import CallbackContext
 
 chat_id = get_config("CI_CHANNEL_ID")
@@ -15,7 +16,14 @@ def update_ci_post(context: CallbackContext, message_id: int,
 	text += "\n"
 	if artifacts is not None:
 		text += artifacts.get_readable_artifacts_list()
-	if message_id is None:
-		return context.bot.send_message(get_config("CI_CHANNEL_ID"), text).message_id
-	else:
-		return context.bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=text)
+	# FIXME:
+	# telegram.vendor.ptb_urllib3.urllib3.exceptions.ReadTimeoutError:
+	# HTTPSConnectionPool(host='api.telegram.org', port=443):
+	# Read timed out. (read timeout=5.0)
+	try:
+		if message_id is None:
+			return context.bot.send_message(get_config("CI_CHANNEL_ID"), text).message_id
+		else:
+			return context.bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=text)
+	except TimedOut:
+		pass
