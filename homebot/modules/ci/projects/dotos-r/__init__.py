ac874062124cf056cd9a49c89b23c8d6b232b8e4
Alex Damaratski <alexeydomoratsky1@gmail.com>
2021-04-13 14:49:00 +0000

    ci_module: aosp: add dotOS to the party

---
+++ homebot/modules/ci/projects/dotos-r/__init__.py
@@ -0,0 +1,5 @@
+from homebot.modules.ci.projects.dotos import dotOSProject
+
+class Project(dotOSProject):
+	version = "5.0.1"
+	android_version = "11"
---
+++ homebot/modules/ci/projects/dotos/__init__.py
@@ -0,0 +1,9 @@
+from homebot.modules.ci.projects.aosp.project import AOSPProject
+
+class dotOSProject(AOSPProject):
+	name = "dotOS"
+	category = "ROMs"
+	lunch_prefix = "dot"
+	lunch_suffix = "userdebug"
+	build_target = "bacon"
+	artifacts = "dotOS-*.zip"
