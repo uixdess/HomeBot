6f46069b696bc7651be4b7d7b8eb2a9d6e72c523
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-05-10 00:19:03 +0200

    weather_module: Use libweather

--- homebot/modules/weather/__init__.py
+++ homebot/modules/weather/__init__.py
@@ -1,23 +1,10 @@
 """HomeBot weather module."""
 
-from homebot import get_config
 from homebot.core.logging import LOGE
 from homebot.core.modules_manager import ModuleBase
-import requests
+from homebot.lib.libweather import Weather
 from telegram.ext import CallbackContext
 from telegram.update import Update
-
-URL = "https://api.openweathermap.org/data/2.5/weather"
-
-TEMP_UNITS = {
-	"imperial": "F",
-	"metric": "C"
-}
-
-WIND_UNITS = {
-	"imperial": "mph",
-	"metric": "km/h"
-}
 
 class Module(ModuleBase):
 	name = "weather"
@@ -30,21 +17,17 @@ class Module(ModuleBase):
 		except IndexError:
 			update.message.reply_text("City not provided")
 			return
-		if get_config("WEATHER_API_KEY", None) == None:
+		try:
+			weather = Weather()
+		except AssertionError:
 			update.message.reply_text("OpenWeatherMap API key not specified\n"
 									  "Ask the bot hoster to configure it")
 			LOGE("OpenWeatherMap API key not specified, get it at https://home.openweathermap.org/api_keys")
 			return
-		parameters = {
-			"appid": get_config("WEATHER_API_KEY", None),
-			"q": city,
-			"units": get_config("WEATHER_TEMP_UNIT", "metric"),
-		}
-		temp_unit = TEMP_UNITS.get(get_config("WEATHER_TEMP_UNIT", None), "K")
-		wind_unit = WIND_UNITS.get(get_config("WEATHER_TEMP_UNIT", None), "km/h")
-		response = requests.get(url=URL, params=parameters).json()
-		if response["cod"] != 200:
-			update.message.reply_text(f"Error: {response['message']}")
+		try:
+			response = weather.current_weather(city)
+		except ConnectionError as err:
+			update.message.reply_text(err.strerror)
 			return
 		city_name = response["name"]
 		city_country = response["sys"]["country"]
@@ -60,9 +43,9 @@ class Module(ModuleBase):
 		update.message.reply_text(
 			f"Current weather for {city_name}, {city_country} ({city_lat}, {city_lon}):\n"
 			f"Weather: {weather_type} ({weather_type_description})\n"
-			f"Temperature: {temp}{temp_unit} (Min: {temp_min}{temp_unit} Max: {temp_max}{temp_unit})\n"
+			f"Temperature: {temp}{weather.temp_unit} (Min: {temp_min}{weather.temp_unit} Max: {temp_max}{weather.temp_unit})\n"
 			f"Humidity: {humidity}%\n"
-			f"Wind: {wind_speed}{wind_unit}"
+			f"Wind: {wind_speed}{weather.wind_unit}"
 		)
 
 	commands = {
